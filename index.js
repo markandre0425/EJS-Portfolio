b7c1707d6ddc8caf6bae444249372b9aca2db51c
markandre0425 <markandre0425@gmail.com>
2025-07-21 01:47:22 +0800

    added views path

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.set("view engine", "ejs")
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+
+console.log("Views directory:", path.join(__dirname, "views"));
+console.log("Current directory:", __dirname);
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
 Step 2: Make sure that static files are linked to and the CSS shows up.
 Step 3: Add the routes to handle the render of the about and contact pages.
-  Hint: Check the nav bar in the header.ejs to see the button hrefs
+  Hint: Check the nav bar in theheader.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
 
